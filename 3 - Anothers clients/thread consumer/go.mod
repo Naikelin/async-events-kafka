6dff3013462865ab011ca4f1d98880a7a04a529b
naikelin <nicolas.nunez2@mail.udp.cl>
2022-10-01 16:53:29 -0300

    Thread consumer

---
+++ 3 - Anothers clients/thread consumer/go.mod
@@ -0,0 +1,10 @@
+module tconsumer
+
+go 1.19
+
+require github.com/segmentio/kafka-go v0.4.35
+
+require (
+	github.com/klauspost/compress v1.15.7 // indirect
+	github.com/pierrec/lz4/v4 v4.1.15 // indirect
+)
---
+++ 3 - Anothers clients/thread consumer/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"sync"
+	"time"
+
+	"github.com/segmentio/kafka-go"
+)
+
+// critical section
+var s []string
+var count int
+var mu sync.Mutex
+
+func printSlice(a []string) {
+	fmt.Printf("len=%d cap=%d %v\n", len(a), cap(a), a)
+}
+
+func saveInSlice(a string) {
+	mu.Lock()
+	defer mu.Unlock()
+	count++
+	s = append(s, a)
+}
+
+func readElements(c chan bool) {
+	r := kafka.NewReader(kafka.ReaderConfig{
+		Brokers:  []string{"localhost:9092"},
+		GroupID:  "CG1",
+		Topic:    "register-event",
+		MinBytes: 10e3, // 10KB
+		MaxBytes: 10e6, // 10MB
+	})
+
+	// with deadline 30secs
+	// ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+
+	ctx := context.Background()
+	for {
+		m, err := r.FetchMessage(ctx)
+		if err != nil {
+			log.Fatal("failed to fetch message:", err)
+			break
+		}
+
+		if err := r.CommitMessages(ctx, m); err != nil {
+			log.Fatal("failed to commit message:", err)
+			break
+		}
+
+		saveInSlice(string(m.Value))
+	}
+
+	c <- true
+
+}
+
+func main() {
+	c := make(chan bool)
+	go readElements(c)
+
+	for {
+		time.Sleep(time.Second * 10)
+		printSlice(s)
+	}
+}
